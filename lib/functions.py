ad66f437e1f48a0ecad079886a490545d63d5afb
Techbandit001 <davidkiptum80@gmail.com>
2025-05-07 19:56:29 +0300

    pyfunctions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
+    return greet_programmer
     pass
 
-def greet(name):
+def greet(Guido):
+    print(f"Hello, {Guido}!")
+    return f"greet"
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+    return f"greet_with_default"
     pass
 
 def add(num1, num2):
+    return num1 + num2
     pass
 
 def halve(number):
+    return number/2
     pass
